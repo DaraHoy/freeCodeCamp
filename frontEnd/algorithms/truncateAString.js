c7a52724ff4706a19c963e346b38b2d11703e6ca
Dara Hoy <darahoy@gmail.com>
2017-11-20 00:48:08 +0000

    Complete Truncate a String challenge

--- frontEnd/algorithms/truncateAString.js
+++ frontEnd/algorithms/truncateAString.js
@@ -5,17 +5,21 @@
 // However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string.
 
 function truncateString(str, num) {
-    if (str.length <= 3) {
-
+    var truncated;
+    if (str.length <= num) {
+        return str;
     }
-    if (str.length > num) {
-        var truncated = str.split("").splice(0, num).join("") + "...";
-        console.log(truncated);
+    else if (num <= 3) {
+        truncated = str.split("").splice(0, num).join("") + "...";
         return truncated;
     }
-    // if str > num
-    // str.s  substr(0, )
-    return str;
+    else if (str.length + 3 > num) {
+        truncated = str.split("").splice(0, num - 3).join("") + "...";
+        return truncated;
+    }
 }
 
-console.log(truncateString("A-tisket a-tasket A green and yellow basket", 11));
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 11)); //should return "A-tisket...".
+console.log(truncateString("Absolutely Longer", 2)); //should return "Ab...".
+console.log(truncateString("A-", 1)); //should return "A...".
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2)) //should return "A-tisket a-tasket A green and yellow basket".
