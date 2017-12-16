0a09046f8583022b1467e7376266c4f7320a1cf1
Dara Hoy <darahoy@gmail.com>
2017-12-16 21:11:40 +0000

    Complete Diff Two Arrays challenge

---
+++ frontEnd/intermediate/diffTwo.js
@@ -0,0 +1,21 @@
+/*Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.*/
+
+/*
+loop through items of arr1 and check for occurence in arr 2 abd vice versa
+return array of differences
+
+*/
+// function diffArray(arr1, arr2) {
+//     var newArr = arr1.concat(arr2);
+//     // newArr.push(arr1.filter(el => arr2.indexOf(el) == -1))
+//     return newArr.filter(el => arr1.indexOf(el) == -1 && arr2.indexOf(el) !== -1 ||
+//         arr2.indexOf(el) == -1 && arr1.indexOf(el) !== -1)
+// }
+
+var diffArray = (arr1, arr2) => arr1.concat(arr2).filter(el => arr1.indexOf(el) == -1 && arr2.indexOf(el) !== -1 || arr2.indexOf(el) == -1 && arr1.indexOf(el) !== -1)
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+//["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
+//["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
