c3e751150c2f6558389ab12cc320d7947be3b08d
Dara Hoy <darahoy@gmail.com>
2017-12-23 14:45:41 +0000

    Complete Drop It challenge

--- frontEnd/intermediate/dropIt.js
+++ frontEnd/intermediate/dropIt.js
@@ -6,9 +6,20 @@ Return the rest of the array, otherwise return an empty array.*/
 
 function dropElements(arr, func) {
     // Drop them elements.
-    return arr.filter(func);
+    for (var i = 0; i < arr.length; i++) {
+        if (!func(arr[i])) { //if func returns true false for item it is dropped from arr
+            arr.shift();
+            i--; //i is decremented to check new arr[0]
+        }
+        else {
+            return arr; //if func arr[i] returns true, arr is returned
+        }
+    }
+    return arr; // returns arr if all items return false
 }
 //dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
 console.log(dropElements([1, 2, 3, 4], function(n) { return n >= 3; }));
 //dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1]
 console.log(dropElements([0, 1, 0, 1], function(n) { return n === 1; }));
+//dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
+console.log(dropElements([1, 2, 3, 4], function(n) { return n > 5; }))
