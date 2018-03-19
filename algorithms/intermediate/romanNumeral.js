e5d26922b46fbda345bf2b3a8d0b97a15dcedcaf
DaraHoy <darahoy@gmail.com>
2018-03-19 13:54:33 -0400

    Add solution to roman numeral

--- algorithms/intermediate/romanNumeral.js
+++ algorithms/intermediate/romanNumeral.js
@@ -1,31 +1,32 @@
-/*Convert the given number into a roman numeral.
+//Convert the given number into a roman numeral.
 
-All roman numerals answers should be provided in upper-case.*/
+function convertToRoman(num) {
+    const romanMap = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    let romanNumeral = "";
 
-var convertToRoman = function(num) {
-
-    var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    var romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-
-    var romanized = '';
-
-    for (var index = 0; index < decimalValue.length; index++) {
-        while (decimalValue[index] <= num) {
-            romanized += romanNumeral[index];
-            num -= decimalValue[index];
+    while(num > 0){
+        for(char in romanMap){
+            if (num >= romanMap[char]){
+                romanNumeral += char;
+                num -= romanMap[char];
+                break;
+            }
         }
     }
 
-    return romanized;
-}
-
-//convertToRoman(2) should return "II".
-console.log(convertToRoman(2))
-//convertToRoman(12) should return "XII".
-console.log(convertToRoman(12))
-//convertToRoman(68) should return "LXVIII"
-console.log(convertToRoman(68))
-//convertToRoman(649) should return "DCXLIX"
-console.log(convertToRoman(649))
-//convertToRoman(3999) should return "MMMCMXCIX"
-console.log(convertToRoman(3999))
+    return romanNumeral;
+   }
